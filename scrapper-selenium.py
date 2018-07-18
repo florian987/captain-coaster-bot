34fe98a2370f3debdbae1beebbc72fc9ffcdaa50
jeff <jeff@hsfactory.net>
2018-07-18 22:47:11 +0200

    Scrapping car page

--- scrapper-selenium.py
+++ scrapper-selenium.py
@@ -41,64 +41,90 @@ proxy = Proxy({
 #            cookie = dict(JSESSIONID=line.split("=")[1])
 #            print(cookie)
 
-
+# Initialize selenium driver
 driver = webdriver.Chrome()
-#driver = webdriver.Firefox(proxy=proxy)
+#driver = webdriver.Firefox(proxy=proxy) # Ajout du proxy
+
 #driver.implicitly_wait(10) # seconds
 #driver.get("http://virtualracingschool.appspot.com/#/DataPacks")
 #driver.add_cookie({"host":"virtualracingschool.appspot.com","domain":"virtualracingschool.appspot.com","secure":False,"expire":1533023830,"name":"vrs","value":"zkXqnElNVioRWuUK1JgojA"})
-driver.get("https://virtualracingschool.appspot.com/#/DataPacks")
 
-time.sleep(10)
+def build_cars_list():
+    driver.get("https://virtualracingschool.appspot.com/#/DataPacks")
 
-row_nodes = driver.find_elements_by_xpath("//table[@data-vrs-widget='tableWrapper']/tbody/tr")
+    # Wait dégueulasse pour load le JS
+    time.sleep(3)
 
-#print(row_nodes)
+    # Retrieve cars table
+    row_nodes = driver.find_elements_by_xpath("//table[@data-vrs-widget='tableWrapper']/tbody/tr")
 
-iracing_series = []
+    #print(row_nodes)
 
-for row in row_nodes:
-    #print(row.find_element_by_css_selector('td:nth-of-type(1)'))
-    #print(row.find_element_by_css_selector('td:nth-of-type(3)').text)
-    
-    node_serie = row.find_element_by_css_selector('td:nth-of-type(1) img').get_attribute('title')
-    node_car = row.find_element_by_css_selector('td:nth-of-type(2) img').get_attribute('title')
-    node_season = row.find_element_by_css_selector('td:nth-of-type(3)').text
-    node_author = row.find_element_by_css_selector('td:nth-of-type(4) img').get_attribute('title')
-    #print(serie_node)
+    # Initialize cars array
+    iracing_cars = []
 
-    print(row.find_element_by_xpath("//span[@data-vrs-widget-field='packIdElement']").get_attribute("innerHTML"))
-    print(row.find_element_by_css_selector("p:nth-of-type(1)").get_attribute("innerHTML"))
+    # Iterate over table rows
+    for row in row_nodes:
+        #print(row.find_element_by_css_selector('td:nth-of-type(1)'))
+        #print(row.find_element_by_css_selector('td:nth-of-type(3)').text)
+        
+        # Retrieve cars infos
+        node_serie = row.find_element_by_css_selector('td:nth-of-type(1) img').get_attribute('title')
+        node_car = row.find_element_by_css_selector('td:nth-of-type(2) img').get_attribute('title')
+        node_season = row.find_element_by_css_selector('td:nth-of-type(3)').text
+        node_author = row.find_element_by_css_selector('td:nth-of-type(4) img').get_attribute('title')
+        #print(serie_node)
 
-    #iracing_series.append(serie_node)
+        # Set premium status depending of style display
+        if "visible" in row.find_element_by_class_name('blue').get_attribute('style'):
+            node_premium = True
+        else:
+            node_premium = False
 
-    datapack = {}
-    datapack['serie'] = node_serie
-    datapack['car'] = node_car
-    datapack['season'] = node_season
-    datapack['author'] = node_author
+        #print(row.find_element_by_xpath("//span[@data-vrs-widget-field='packIdElement']").get_attribute("innerHTML"))
 
-    print(json.dumps(datapack, indent=4))
+        # Get car ID
+        node_span = row.find_element_by_css_selector("p:nth-of-type(1)").get_attribute("innerHTML")
+        soup = BeautifulSoup(node_span, "html.parser")
+        for span in soup.findAll("span", attrs={"data-vrs-widget-field":"packIdElement"}):
+            node_id = span.text
 
-    #print(img_node.get_attribute('src'), img_node.get_attribute('title'), img_node.get_attribute('alt'))
+        #iracing_series.append(serie_node)
 
-#print(json.dumps(datapack, indent=4))
+        # Create car dict
+        datapack = {}
+        datapack['serie'] = node_serie
+        datapack['car'] = node_car
+        datapack['season'] = node_season
+        datapack['author'] = node_author
+        datapack['id'] = node_id
+        datapack['premium'] = node_premium
 
-#html = driver.page_source
+        iracing_cars.append(datapack)
 
-#wait = WebDriverWait(driver, 15)
-#element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'card-content')))
+        #print(json.dumps(datapack, indent=4))
 
-#html = driver.execute_script('return document.documentElement.outerHTML')
+        #print(img_node.get_attribute('src'), img_node.get_attribute('title'), img_node.get_attribute('alt'))
 
-#soup = BeautifulSoup(html, 'lxml')
+    # debug cars list
+    print(json.dumps(iracing_cars, indent=4))
 
-#print(html)
+    # Return cars list
+    return iracing_cars
 
-#for line in html:
-#    if "25590001" in line:
-#        print('SAMARCHE')
+cars_list = build_cars_list()
 
+for car in cars_list:
+    if car['premium'] == False:
+        driver.get("https://virtualracingschool.appspot.com/#/DataPacks/" + car['id'])
+
+        # Wait dégueulasse pour load le JS
+        time.sleep(3)
+
+        # Retrieve cars table
+        row_nodes = driver.find_elements_by_xpath("//table[@data-vrs-widget='DataPackWeeksTable']/tbody/tr")
+
+# Implicit selenium wait test
 #try:
 #    element = WebDriverWait(driver, 10).until(
 #        EC.presence_of_element_located((By.ID, "myDynamicElement"))

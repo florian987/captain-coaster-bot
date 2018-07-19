63e86370d4f700029622750be7a49fc0b88f03d0
jeff <jeff@hsfactory.net>
2018-07-19 20:25:17 +0200

    dapush

--- scrapper-selenium.py
+++ scrapper-selenium.py
@@ -24,6 +24,12 @@ proxy = Proxy({
     'noProxy': '' # set this value as desired
     })
 
+profile = webdriver.FirefoxProfile()
+profile.set_preference('browser.download.folderList', 2) # custom location
+profile.set_preference('browser.download.manager.showWhenStarting', False)
+profile.set_preference('browser.download.dir', './downloads/')
+profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream')
+profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/zip')
 
 
 # Get cookie from file
@@ -35,7 +41,7 @@ proxy = Proxy({
 
 
 # Initialize selenium driver
-driver = webdriver.Chrome()
+driver = webdriver.Firefox(profile)
 #driver = webdriver.Firefox(proxy=proxy) # Ajout du proxy
 
 #driver.implicitly_wait(10) # seconds
@@ -49,27 +55,27 @@ def build_cars_list():
     time.sleep(3)
 
     # Retrieve cars table
-    row_nodes = driver.find_elements_by_xpath("//table[@data-vrs-widget='tableWrapper']/tbody/tr")
+    cars_summary = driver.find_elements_by_xpath("//table[@data-vrs-widget='tableWrapper']/tbody/tr")
 
-    #print(row_nodes)
+    #print(cars_summary)
 
     # Initialize cars array
     iracing_cars = []
 
-    # Iterate over table rows
-    for row in row_nodes:
+    # Iterate over table car_elements
+    for car_element in cars_summary:
         #print(row.find_element_by_css_selector('td:nth-of-type(1)'))
         #print(row.find_element_by_css_selector('td:nth-of-type(3)').text)
         
         # Retrieve cars infos
-        node_serie = row.find_element_by_css_selector('td:nth-of-type(1) img').get_attribute('title')
-        node_car = row.find_element_by_css_selector('td:nth-of-type(2) img').get_attribute('title')
-        node_season = row.find_element_by_css_selector('td:nth-of-type(3)').text
-        node_author = row.find_element_by_css_selector('td:nth-of-type(4) img').get_attribute('title')
+        node_serie = car_element.find_element_by_css_selector('td:nth-of-type(1) img').get_attribute('title')
+        node_car = car_element.find_element_by_css_selector('td:nth-of-type(2) img').get_attribute('title')
+        node_season = car_element.find_element_by_css_selector('td:nth-of-type(3)').text
+        node_author = car_element.find_element_by_css_selector('td:nth-of-type(4) img').get_attribute('title')
         #print(serie_node)
 
         # Set premium status depending of style display
-        if "visible" in row.find_element_by_class_name('blue').get_attribute('style'):
+        if "visible" in car_element.find_element_by_class_name('blue').get_attribute('style'):
             node_premium = True
         else:
             node_premium = False
@@ -77,7 +83,7 @@ def build_cars_list():
         #print(row.find_element_by_xpath("//span[@data-vrs-widget-field='packIdElement']").get_attribute("innerHTML"))
 
         # Get car ID
-        node_span = row.find_element_by_css_selector("p:nth-of-type(1)").get_attribute("innerHTML")
+        node_span = car_element.find_element_by_css_selector("p:nth-of-type(1)").get_attribute("innerHTML")
         soup = BeautifulSoup(node_span, "html.parser")
         for span in soup.findAll("span", attrs={"data-vrs-widget-field":"packIdElement"}):
             node_id = span.text
@@ -120,57 +126,78 @@ for car in cars_list:
         time.sleep(3)
 
         # Retrieve cars table
-        row_nodes = driver.find_elements_by_xpath("//table[@data-vrs-widget='DataPackWeeksTable']/tbody/tr")
+        cars_summary = driver.find_elements_by_xpath("//table[@data-vrs-widget='DataPackWeeksTable']/tbody/tr")
 
-        for row in row_nodes:
+        for car_element in cars_summary:
 
             # Create datapacks list
             datapack = {}
 
+            # Build datapack (only one registered atm)
 
             try:
                 # Build fom tracks lists
-                datapack['track'] = row.find_element_by_css_selector('td:nth-of-type(2) img').get_attribute('title')
-                datapack['fastest_laptime'] = row.find_element_by_css_selector('td:nth-of-type(3) span:nth-of-type(1) span:nth-of-type(1)').get_attribute('title')
-                datapack['time_of_day'] = row.find_element_by_css_selector('td:nth-of-type(4) span:nth-of-type(1) span').get_attribute('title')
-                datapack['track_state'] = row.find_element_by_css_selector('td:nth-of-type(4) span:nth-of-type(2) span').get_attribute('title')
+                datapack['track'] = car_element.find_element_by_css_selector('td:nth-of-type(2) img').get_attribute('title')
+                datapack['fastest_laptime'] = car_element.find_element_by_css_selector('td:nth-of-type(3) span:nth-of-type(1) span:nth-of-type(1)').get_attribute('title')
+                datapack['time_of_day'] = car_element.find_element_by_css_selector('td:nth-of-type(4) span:nth-of-type(1) span').get_attribute('title')
+                datapack['track_state'] = car_element.find_element_by_css_selector('td:nth-of-type(4) span:nth-of-type(2) span').get_attribute('title')
 
                 car['datapacks'].append(datapack)
 
                 #print(datapack)
 
-                # GoTo DataPack Page
-                goto_datapack = row.find_element_by_css_selector('td:nth-of-type(7) a').click()
-
-                goto_datapack
-
-                time.sleep(3)
-
-                session_files = driver.find_elements_by_css_selector("div.vrs-list-view")
-                print(len(session_files))
-                print(session_files.get_attribute("innerHTML"))
-
-                for session_row in session_files:
-                    file_name = session_row.find_elements_by_css_selector("form.session-file-name div a ").text()
-                    file_grab = session_row.find_elements_by_css_selector("form.session-file-name div a").click()
-
-                    print(file_name)
-                driver.back()
-
-
-                car['datapacks'].append(datapack)
-
             except:
                 continue
 
+            if datapack['fastest_laptime'] != "":
+                car_element.find_element_by_css_selector('td:nth-of-type(7) a').click()
+                time.sleep(3)
+
+                datapack_files = []
+                for line in driver.find_elements_by_css_selector("form.session-file-name"):
+                    for file in line.find_elements_by_css_selector("a.gwt-Anchor"):
+
+                        # Download file
+                        file.click
+
+                        # Build filename_list
+                        filename = str(file.text).replace(' ','_').lower()
+
+                        datapack_file = {}
+                        datapack_file['name'] = filename
+                        datapack_files.append(datapack_files)
+                
+                print(datapack_files)
+
+
+            # GoTo DataPack Page
+            # if datapck not empty ?
+            #goto_datapack = car_element.find_element_by_css_selector('td:nth-of-type(7) a').click()
+#
+            #goto_datapack
+#
+            #time.sleep(3)
+#
+            #session_files = driver.find_elements_by_css_selector("div.vrs-list-view")
+            #print(len(session_files))
+            #print(session_files)
+            #for session_row in session_files:
+            #    print(dir(session_row.find_elements_by_css_selector("form.session-file-name div a ")))
+            #    #file_name = session_row.find_elements_by_css_selector("form.session-file-name div a ").text
+            #    #file_grab = session_row.find_elements_by_css_selector("form.session-file-name div a").click()
+##
+            #    #print(file_name)
+            #    
+            #driver.back()
+            #time.sleep(3)
             
 
-            #goto_datapack = row.find_element_by_css_selector('td:nth-of-type(7) a').click()
+            #goto_datapack = car_element.find_element_by_css_selector('td:nth-of-type(7) a').click()
 
 
-            #datapack['track'] = row.find_element_by_xpath("//td[2]/div/span[1]").get_attribute("innerHTML")
+            #datapack['track'] = car_element.find_element_by_xpath("//td[2]/div/span[1]").get_attribute("innerHTML")
 
-            #laptime_span = row.find_element_by_css_selector("td:nth-of-type(3)").get_attribute("innerHTML")
+            #laptime_span = car_element.find_element_by_css_selector("td:nth-of-type(3)").get_attribute("innerHTML")
             #soup = BeautifulSoup(node_span, "html.parser")
             #for span in soup.findAll("span", attrs={"data-vrs-widget-field":"packIdElement"}):
             #    node_id = span.text

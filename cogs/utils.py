71a1a4a424a63c68f0988b0eb97746381e67a237
jeff <jeff@hsfactory.net>
2018-08-02 18:29:25 +0200

    utils.py placeholder

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,42 +1,63 @@
+from discord.ext import commands
 import discord
 
-def build_embed(**kwargs):
 
-            title = kwargs.pop('title', "Titre")
-            descr = kwargs.pop('descr', "Description")
-            colour = kwargs.pop('colour', ctx.author.color)
-            img = kwargs.pop('img', None)
-            author_icon = kwargs.pop('author_icon', ctx.author.avatar_url)
-            author_name = kwargs.pop('author_icon', ctx.author.name)
-            author_url = kwargs.pop('author_url', ctx.author.avatar_url)
-            print_dict = kwargs.pop('print_dict', False)
-            footer_text = kwargs.pop('footer_text', None)
-            footer_icon = kwargs.pop('footer_icon', None)
-            content = ''
+def build_embed(ctx, **kwargs):
 
-            embed = discord.Embed(
-                title=title,
-                description=descr,
-                colour=colour
-            )
+    parameters = {
+        'author_avatar_url': ctx.author.avatar_url,
+        'author_name': ctx.author.name,
+        'author_url': ctx.author.url,
+        'colour': discord.Colour.default(),
+        'img_url': None,
+        'footer_text': None,
+        'footer_icon': None,
+        'title': 'Title',
+        'description': 'Description',
+        'content': ''
+    }
 
-            embed.set_author(
-                icon_url=author_icon,
-                name=author_name,
-                url=author_url
-            )
-
-            if img:
-                embed.set_image(url=img)
-
-            if footer_text or footer_icon:
-                embed.set_footer(text=footer_text,icon_url=footer_icon)
-
-            for key, value in kwargs.items():
-                embed.add_field(name=key, value=value, inline=True)
+    for parameter in parameters.items():
+        if parameter in kwargs:
+            parameters[parameter] = kwargs.pop(parameter)
+            
+    embed = discord.Embed(
+        title = parameters['title'],
+        description = parameters['description'],
+        colour = parameters['colour']
+    )
+    
+    if kwargs:
+        for k, v in kwargs:
+            embed.add_field(name=k, value=v, inline=True)
 
 
-            if print_dict:
-                content = embed.to_dict()
+    if icon_url is None:
+        icon_url=ctx.author.avatar_url
 
-            return content, embed
+    embed = discord.Embed(
+        title=title,
+        description=descr,
+        colour=colour
+    )
+
+    embed.set_author(
+        icon_url=ctx.author.avatar_url,
+        name=author_name,
+        url=author_url
+    )
+
+    if img:
+        embed.set_image(url=img)
+
+    if footer_text or footer_icon:
+        embed.set_footer(text=footer_text,icon_url=footer_icon)
+
+    for key, value in kwargs.items():
+        embed.add_field(name=key, value=value, inline=True)
+
+
+    if print_dict:
+        content = embed.to_dict()
+
+    return content, embed

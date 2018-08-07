55cb53522df4081163ea2202c813fa61188e22a9
Geoffrey Bachelot <geoffrey.bachelot@bnf.fr>
2018-08-07 14:53:16 +0200

    verb3

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -32,6 +32,7 @@ class CommandErrorHandler:
         error : Exception"""
 
         print(dir(error))
+        print(error)
 
         # This prevents any commands with local handlers being handled here in on_command_error.
         if hasattr(ctx.command, 'on_error'):

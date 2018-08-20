2fa255fb08529022aadbada02fc78a35eebcd018
jeff <jeff@hsfactory.net>
2018-08-20 10:19:35 +0200

    fix ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,13 @@ if [[ $CI_COMMIT_REF_SLUG == 'master' ]]; then
     #echo "Connecting to docker hub"
     #echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-    ci_changed_lines=$(git diff HEAD~1 HEAD docker/Dockerfile.base | wc -l)
+    ci_changed_lines=$(git diff HEAD~1 HEAD docker/ci.Dockerfile | wc -l)
     base_changed_lines=$(git diff HEAD~1 HEAD docker/Dockerfile.base | wc -l)
 
     if [ $ci_changed_lines != '0' ]; then
-      echo "Dockerfile.base was changed"
+      echo "ci.Dockerfile was changed"
 
-      echo "Building bot base"
+      echo "Building bot ci"
       docker build -t hub.hsfactory.net/jeff/discord-bot:ci -f docker/ci.Dockerfile .
 
       echo "Pushing image to Docker Hub"

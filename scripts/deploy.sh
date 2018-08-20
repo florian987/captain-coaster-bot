b265f25bd84ae2980d6157e0975e86a2b2029b78
jeff <jeff@hsfactory.net>
2018-08-20 10:09:33 +0200

    CI fixes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,8 @@
 
 # Build and deploy on master branch
 if [[ $CI_COMMIT_REF_SLUG == 'master' ]]; then
-    echo "Connecting to docker hub"
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    #echo "Connecting to docker hub"
+    #echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
     changed_lines=$(git diff HEAD~1 HEAD docker/Dockerfile.base | wc -l)
 
@@ -11,19 +11,19 @@ if [[ $CI_COMMIT_REF_SLUG == 'master' ]]; then
       echo "Dockerfile.base was changed"
 
       echo "Building bot base"
-      docker build -t pythondiscord/bot-base:latest -f docker/Dockerfile.base .
+      docker build -t hub.hsfactory.net/jeff/discord-bot:base -f docker/Dockerfile.base .
 
       echo "Pushing image to Docker Hub"
-      docker push pythondiscord/bot-base:latest
+      docker push hub.hsfactory.net/jeff/discord-bot:base
     else
       echo "Dockerfile.base was not changed, not building"
     fi
 
     echo "Building image"
-    docker build -t pythondiscord/bot:latest -f docker/Dockerfile .
+    docker build -t hub.hsfactory.net/jeff/discord-bot:latest -f docker/Dockerfile .
 
     echo "Pushing image"
-    docker push pythondiscord/bot:latest
+    docker push hub.hsfactory.net/jeff/discord-bot:latest
 
     echo "Deploying container"
     curl -H "token: $AUTODEPLOY_TOKEN" $AUTODEPLOY_WEBHOOK

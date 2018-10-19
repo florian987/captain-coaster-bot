943e402a4baf26c0f70941c63832095588f29bd0
Geoffrey Bachelot <geoffrey.bachelot@bnf.fr>
2018-10-19 15:22:17 +0200

    débilité

--- bot/cogs/coasters.py
+++ bot/cogs/coasters.py
@@ -230,7 +230,7 @@ class RollerCoasters:
                     description=f"Il s'agissait de {coaster_infos['name']} se trouvant à {coaster_infos['park']['name']}")
                 await ctx.send(embed=embed)
             else:
-                if msg.content.lower() in valid_coaster_answers:
+                if coaster_answers(msg):
                     embed = await build_embed(
                         ctx,
                         colour='green',
